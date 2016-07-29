4eab8c5b820f44d48a853afc580ed612c410d023
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-29 21:43:07 +0200

    Completely redo CI, removing travis

--- .scripts/fix-pypy1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [ "$TRAVIS_PYTHON_VERSION" == "pypy" ]; then
-	sudo rm -rf /opt/pyenv
-	sudo rm -rf $HOME/.pyenv
-	curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-fi
--- .scripts/fix-pypy2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [ "$TRAVIS_PYTHON_VERSION" == "pypy" ]; then
-	pyenv update
-	pyenv install pypy-5.1.1
-	pyenv global pypy-5.1.1
-fi
