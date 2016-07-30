0ea219e4f979e536ec145011ff830966e91525f3
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-30 15:52:39 +0200

    Make with sudo

--- .scripts/travis_run.sh
+++ .scripts/travis_run.sh
@@ -2,7 +2,7 @@
 
 if [ "$BUILDTYPE" == "osx" ]; then
     brew install libffi
-    make test-all
+    sudo make test-all
 else
     docker run -v $PWD:/outside adfinissygroup/chirp-jessie /bin/sh \
         -c "cd /outside && ./.scripts/docker_run.sh $BUILDTYPE"
