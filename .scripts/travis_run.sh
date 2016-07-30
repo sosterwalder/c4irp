dba88261c9caf209fcf97405685e412be0714212
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-30 14:47:24 +0200

    No docker on osx

--- .scripts/travis_run.sh
+++ .scripts/travis_run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -v $PWD:/outside adfinissygroup/chirp-jessie /bin/sh \
-    -c "cd /outside && ./.scripts/docker_run.sh $BUILDTYPE"
+if [ "$BUILDTYPE" == "osx" ]; then
+    make test-all
+else
+    docker run -v $PWD:/outside adfinissygroup/chirp-jessie /bin/sh \
+        -c "cd /outside && ./.scripts/docker_run.sh $BUILDTYPE"
+fi
