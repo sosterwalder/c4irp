a1d6c8c936f06227a45f147110e7d50bdb9dfac4
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-08-07 15:00:28 +0200

    Protected python env from my build environment

--- .scripts/win-build.cmd
+++ .scripts/win-build.cmd
@@ -1,7 +1,8 @@
 @rem Look for Visual Studio 2015
 if not defined VS140COMNTOOLS goto vc-set-2013
 if not exist "%VS140COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-2013
-set VS90COMNTOOLS="%VS140COMNTOOLS%
+call "%VS140COMNTOOLS%\..\..\vc\vcvarsall.bat" %vs_toolset%
+set GYP_MSVS_VERSION=2015
 echo Using Visual Studio 2015
 goto vs-found
 
@@ -9,7 +10,8 @@ goto vs-found
 @rem Look for Visual Studio 2013
 if not defined VS120COMNTOOLS goto vc-set-2012
 if not exist "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-2012
-set VS90COMNTOOLS="%VS120COMNTOOLS%
+call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" %vs_toolset%
+set GYP_MSVS_VERSION=2013
 echo Using Visual Studio 2013
 goto vs-found
 
@@ -17,7 +19,8 @@ goto vs-found
 @rem Look for Visual Studio 2012
 if not defined VS110COMNTOOLS goto vc-set-2010
 if not exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-2010
-set VS90COMNTOOLS="%VS110COMNTOOLS%
+call "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" %vs_toolset%
+set GYP_MSVS_VERSION=2012
 echo Using Visual Studio 2012
 goto vs-found
 
@@ -25,7 +28,8 @@ goto vs-found
 @rem Look for Visual Studio 2010
 if not defined VS100COMNTOOLS goto vc-set-2008
 if not exist "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-2008
-set VS90COMNTOOLS="%VS100COMNTOOLS%
+call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" %vs_toolset%
+set GYP_MSVS_VERSION=2010
 echo Using Visual Studio 2010
 goto vs-found
 
@@ -33,6 +37,8 @@ goto vs-found
 @rem Look for Visual Studio 2008
 if not defined VS90COMNTOOLS goto vc-set-notfound
 if not exist "%VS90COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-notfound
+call "%VS90COMNTOOLS%\..\..\vc\vcvarsall.bat" %vs_toolset%
+set GYP_MSVS_VERSION=2008
 echo Using Visual Studio 2008
 goto vs-found
 
@@ -41,3 +47,6 @@ echo Visual Studio not found
 exit /B 1
 
 :vs-found
+
+copy config.defs.h config.h || exit /B 1
+python .scripts\win-build.py || exit /B 1
