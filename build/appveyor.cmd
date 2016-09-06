894c81f966e3b592504dc81a80949cc69908a5ef
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-09-06 14:33:46 +0200

    Change to build and testbuild

--- build/appveyor.cmd
+++ build/appveyor.cmd
@@ -1,5 +1,3 @@
 set PATH=%PYTHONVER%;%PYTHONVER%\Scripts;%PATH%
 pip install -U cffi || exit /B 1
-pip install -U -e . || exit /B 1
 python setup.py bdist_wheel || exit /B 1
-@REM 7z a build.zip .
--- build/appveyortest.cmd
+++ build/appveyortest.cmd
@@ -1,3 +1,7 @@
 set PATH=%PYTHONVER%;%PYTHONVER%\Scripts;%PATH%
+exit /B 0
+git clean -xdf
+set MODE=debug
+pip install -U -e . || exit /B 1
 pip install -U click freeze hypothesis hypothesis-pytest pytest pytest_catchlog pytest_cov pytest_mock testfixtures || exit /B 1
 py.test --doctest-modules chirp || exit /B 1
