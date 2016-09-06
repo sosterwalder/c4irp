20938552429c9f4618d59fb7e169ade088c9b03b
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-09-06 22:22:44 +0200

    Keep artifacts

--- build/appveyortest.cmd
+++ build/appveyortest.cmd
@@ -1,7 +1,7 @@
 set PATH=%PYTHONVER%;%PYTHONVER%\Scripts;%PATH%
 set PYTHONPATH=%CD%
 if "%TESTLIB%"=="true" goto testlib
-git clean -xdf
+git clean -e /dist -xdf
 set MODE=debug
 pip install -U -e . || exit /B 1
 pip install -U click freeze hypothesis hypothesis-pytest pytest pytest_catchlog pytest_cov pytest_mock testfixtures || exit /B 1
