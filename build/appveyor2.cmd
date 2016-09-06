98087188527ad4aaa8b2e5325d35be8104efbb7e
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-09-06 11:45:11 +0200

    Another go on artifacts

--- build/appveyor2.cmd
+++ build/appveyor2.cmd
@@ -1,3 +1,5 @@
 set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
 pip install -U cffi
-pip install -U -e .  || exit /B 0
+pip install -U -e .
+7z a build.zip .
+exit /B 0
--- build/appveyortest2.cmd
+++ build/appveyortest2.cmd
@@ -1,3 +1,4 @@
 set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
+exit /B 0
 pip install -U click freeze hypothesis hypothesis-pytest pytest pytest_catchlog pytest_cov pytest_mock testfixtures || exit /B 1
 py.test --doctest-modules chirp || exit /B 1
