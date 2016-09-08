e56fb03c55ce126cb0e9e197b6d941d1098fb929
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-09-08 13:35:32 +0200

    Link against openssl from homebrew on OS X

--- build/travis_run.sh
+++ build/travis_run.sh
@@ -4,13 +4,19 @@ set -e
 
 ./make.py > /dev/null
 if [ "$BUILDTYPE" == "osx-test" ]; then
-    brew install libffi cppcheck
+    brew update
+    brew install libffi cppcheck openssl
+    brew link --force openssl
     NOLIB=true sudo ./make.py test
 elif [ "$BUILDTYPE" == "osx-lib" ]; then
-    brew install libffi
+    brew update
+    brew install libffi cppcheck openssl
+    brew link --force openssl
     NOLIB=false sudo ./make.py test-lib
 elif [ "$BUILDTYPE" == "osx-release" ]; then
-    brew install libffi
+    brew update
+    brew install libffi cppcheck openssl
+    brew link --force openssl
     sudo python setup.py install
 else
     docker run -v $PWD:/outside adfinissygroup/chirp-jessie /bin/sh \
