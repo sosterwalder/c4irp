5a1f4d4b272fb9fa99af48f03734932bc2c996ea
Sven <sven.osterwalder@gmail.com>
2016-09-09 12:39:37 +0200

    Uninstall/Install libtool before installing other dependencies
    According to 1) it is a known issue with libtool/sed using homebrew.
    Re-installing libtool seems to fix that issue.
    
    1) https://github.com/Homebrew/legacy-homebrew/issues/43874

--- build/travis_run.sh
+++ build/travis_run.sh
@@ -5,15 +5,18 @@ set -e
 ./make.py > /dev/null
 if [ "$BUILDTYPE" == "osx-test" ]; then
     brew update
-    brew install libffi cppcheck openssl libtool
+    brew uninstall libtool && brew install libtool
+    brew install libffi cppcheck openssl
     NOLIB=true sudo ./make.py test
 elif [ "$BUILDTYPE" == "osx-lib" ]; then
     brew update
-    brew install libffi cppcheck openssl libtool
+    brew uninstall libtool && brew install libtool
+    brew install libffi cppcheck openssl
     NOLIB=false sudo ./make.py test-lib
 elif [ "$BUILDTYPE" == "osx-release" ]; then
     brew update
-    brew install libffi cppcheck openssl libtool
+    brew uninstall libtool && brew install libtool
+    brew install libffi cppcheck openssl
     sudo python setup.py install
 else
     docker run -v $PWD:/outside adfinissygroup/chirp-jessie /bin/sh \
