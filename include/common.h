7e23fe61c9c3cff2e3781f0fca21016ac6b99a62
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-09-26 22:52:42 +0200

    Change common.h according to RULES.rst

--- include/common.h
+++ include/common.h
@@ -21,59 +21,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
-#ifdef _WIN32
-#   if defined(_MSC_VER) && _MSC_VER < 1600
-#       include <stdint-msvc2008.h>
-#       define ch_inline __inline
-#   else // _MSC_VER
-#       include <stdint.h>
-#       define ch_inline inline
-#   endif // _MSC_VER
-
-#   if defined(_MSC_VER) && _MSC_VER < 1900
-
-#       define snprintf c99_snprintf
-#       define vsnprintf c99_vsnprintf
-
-        __inline int c99_vsnprintf(
-                char *outBuf,
-                size_t size,
-                const char *format,
-                va_list ap
-        )
-        {
-            int count = -1;
-
-            if (size != 0)
-                count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
-            if (count == -1)
-                count = _vscprintf(format, ap);
-
-            return count;
-        }
-
-        __inline int c99_snprintf(
-                char *outBuf,
-                size_t size,
-                const char *format,
-                ...
-        )
-        {
-            int count;
-            va_list ap;
-
-            va_start(ap, format);
-            count = c99_vsnprintf(outBuf, size, format, ap);
-            va_end(ap);
-
-            return count;
-        }
-
-#   endif // MSVC
-#else // _WIN32
-#   define ch_inline inline
-#endif // _WIN32
-
 // Logging and assert macros
 // =========================
 //
@@ -135,5 +82,58 @@
 ch_chirp_t* chirp = (ch_chirp_t*) handle->data; \
 A(chirp->_init == CH_CHIRP_MAGIC, "Not a ch_chirp_t*")
 /* Fail fast, best I know :( */
+
+#ifdef _WIN32
+#   if defined(_MSC_VER) && _MSC_VER < 1600
+#       include <stdint-msvc2008.h>
+#       define ch_inline __inline
+#   else // _MSC_VER
+#       include <stdint.h>
+#       define ch_inline inline
+#   endif // _MSC_VER
+
+#   if defined(_MSC_VER) && _MSC_VER < 1900
+
+#       define snprintf c99_snprintf
+#       define vsnprintf c99_vsnprintf
+
+        __inline int c99_vsnprintf(
+                char *outBuf,
+                size_t size,
+                const char *format,
+                va_list ap
+        )
+        {
+            int count = -1;
+
+            if (size != 0)
+                count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+            if (count == -1)
+                count = _vscprintf(format, ap);
+
+            return count;
+        }
+
+        __inline int c99_snprintf(
+                char *outBuf,
+                size_t size,
+                const char *format,
+                ...
+        )
+        {
+            int count;
+            va_list ap;
+
+            va_start(ap, format);
+            count = c99_vsnprintf(outBuf, size, format, ap);
+            va_end(ap);
+
+            return count;
+        }
+
+#   endif // MSVC
+#else // _WIN32
+#   define ch_inline inline
+#endif // _WIN32
 
 #endif //ch_common_h
